3e3bd67281ef7e001d9b90e901b2b0f455751af9
Marcos Duran <Marcos_Duran@ucne.edu.do>
2024-02-12 18:22:01 -0400

    Creando el modelo de metas

---
+++ Parcial1_Ap1_MarcosDuran/Models/Metas.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+public class Clientes
+{
+    [Key]
+    public int MetasId { get; set; }
+
+    [Required(ErrorMessage = "El campo fecha es requerido")]
+    public string Fecha { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "La descripcion telefono es requerida")]
+    public string Descripcion { get; set; }
+
+    [Required(ErrorMessage = "El monto requerido")]
+    public int Monto { get; set; }
+
+}
